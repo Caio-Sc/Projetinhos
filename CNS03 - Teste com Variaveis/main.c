9d6d559e246cca39a21931094b657e9417fb2ae0
pudimxd <53196995+pudimxd@users.noreply.github.com>
2021-03-11 12:05:52 -0300

    Atividade 3

---
+++ CNS03 - Teste com Variaveis/main.c
@@ -0,0 +1,19 @@
+//CNS03 - Teste com váriaveis
+//Caio Sclavi 11/03/21
+//
+#include <stdio.h>
+
+int main()
+{
+    int A, B;
+    float Nota1, Nota2, Nota3, Media;
+    A = 10;
+    B = 30;
+    Nota1 = 5.5;
+    Nota2 = 7.3;
+    Nota3 = 9.0;
+    Media = (Nota1 + Nota2 + Nota3) / 3;
+    printf("A media do aluno foi: %.1f\n", Media);
+    printf("Somando %d com %d temos o resultado %d\n", A, B, A + B);
+    return 0;
+}
