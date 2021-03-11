b13ba3715ac0597de6306cb781f6e514b3dc4726
pudimxd <53196995+pudimxd@users.noreply.github.com>
2021-03-11 13:08:50 -0300

    atitivade 4

--- Aprendendo/CNS04 - Variaveis input/main.c
+++ Aprendendo/CNS04 - Variaveis input/main.c
@@ -6,12 +6,14 @@
 int main()
 {
     float n1, n2, media;
-    printf("Defina a primeira nota:");
+    printf("========================\n"
+           "Defina a primeira nota:");
     scanf("%f", &n1);
     printf("Defina a segunda nota:");
     scanf("%f", &n2);
     media = (n1 + n2) / 2;
     printf("Media Final: %.1f\n", media);
+    printf("========================\n");
     system("pause");
     return 0;
 }
