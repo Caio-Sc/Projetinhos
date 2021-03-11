c9fb128cd38dd50473215bcd3d419643cd50c8cc
pudimxd <53196995+pudimxd@users.noreply.github.com>
2021-03-11 11:41:53 -0300

    Atividade 2

---
+++ CNS02/main.c
@@ -0,0 +1,11 @@
+//CNS02
+//Caio Sclavi 11/03/21
+//
+#include <stdio.h>
+int main() {
+    
+    printf( "========================\n"
+            "TESTEEEEE\n"
+            "========================\n");
+    return 0;
+}
