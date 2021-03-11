96faff8d054cbe44ea04caab6aa08f73245ab69b
pudimxd <53196995+pudimxd@users.noreply.github.com>
2021-03-11 12:05:08 -0300

    Atividade 1

--- CNS02 - Printf mesma linha/main.c
+++ CNS02 - Printf mesma linha/main.c
@@ -1,8 +1,12 @@
-//CNS01 Primeiro programa em C
-//Caio Sclavi 04/03/21
-#include <stdio.h> //biblioteca de input/output padrão 
+//CNS02 - Printf mesma linha
+//Caio Sclavi 11/03/21
+//
+#include <stdio.h>
 int main()
 {
-    printf("Testando printf\nOla professor\n");
-    return 0;   
+
+    printf("========================\n"
+           "TESTEEEEE\n"
+           "========================\n");
+    return 0;
 }
