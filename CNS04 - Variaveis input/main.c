0d95d015904b40e3ab9b15748e02b468606cad41
pudimxd <53196995+pudimxd@users.noreply.github.com>
2021-03-11 12:25:04 -0300

    Atividade 4

---
+++ CNS04 - Variaveis input/main.c
@@ -0,0 +1,16 @@
+//CNS03 - Variaveis input
+//Caio Sclavi 11/03/21
+//
+#include <stdio.h>
+
+int main()
+{
+    float n1, n2, media;
+    printf("Defina a primeira nota:");
+    scanf("%f", &n1);
+    printf("Defina a segunda nota:");
+    scanf("%f", &n2);
+    media = (n1 + n2) / 2;
+    printf("Media Final: %.1f", media);
+    return 0;
+}
