edee9bd53f4e3330be24876574c9cf6b5701bac4
Caio Sclavi <pudimxd1@gmail.com>
2021-05-09 16:18:46 -0300

    Atividades

---
+++ Atividades/Experimento 6/CNS_Exp06-1.c
@@ -0,0 +1,17 @@
+//CNS03 - Exp1 - Exercicio 3 (convertesor de temperatura)
+//Caio Sclavi 11/03/21
+//
+#include <stdio.h>
+
+int main()
+{
+       float f, c;
+       printf("====================================================\n"
+              "Coloque o valor em Fahrenheit para ser convertido:");
+       scanf("%f", &f);
+       c = ((f - 32) / 1.8); //Faz a conversão de graus Fahrenheit para graus Celsius
+       printf("%f Celsius\n"
+              "====================================================\n",
+              c);
+       system("pause");
+}
